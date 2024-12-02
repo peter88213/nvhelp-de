3cbca0d6f245931bf9bdc91665211a91ff16d26b
peter88213 <petert88213@gmail.com>
2024-12-02 10:31:20 +0100

    Update docstring.

--- help_de.py
+++ help_de.py
@@ -1,7 +1,7 @@
 """Translate GUI terms in helpfiles to German 
 
 Copyright (c) 2023 Peter Triesberger
-For further information see https://github.com/peter88213/
+For further information see https://github.com/peter88213/nvhelp_de
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 import os
